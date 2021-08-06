84b2f0f29f01518baa12eefc2a2884baf7179608
Soren-netka <75098777+Soren-netka@users.noreply.github.com>
2021-08-05 22:14:26 -0700

    Optimized Client
    Changed textbox asset

--- TunnelClient.py
+++ TunnelClient.py
@@ -1,4 +1,4 @@
-import cv2, pyautogui, time, pyperclip, os, base64, socket
+import pyautogui, time, pyperclip, os, base64, socket, numpy
 
 """
 NOTES
@@ -20,31 +20,19 @@ BUFFER_SIZE = 12048
 def find_coords():
     '''Runs on startup to find essential GUI object coordinates'''
 
-    # Take the screenshot to look for coords in
-    pyautogui.screenshot('./assets/ss.png')
-
     # Resize the WhatsApp window
     resize()
-    os.remove('./assets/ss.png')
-    pyautogui.screenshot('./assets/ss.png')
     
     # Find coords of gui objects
     find_new_message()
     find_text_box()
 
-    os.remove('assets/ss.png')
-
 
 def resize():
     '''Resize the WhatsApp window to make it easier to find the newest message coords easier'''
-
+    
     # Get coords to change window size
-    method = cv2.TM_SQDIFF_NORMED
-    small_image = cv2.imread('./assets/resize.png')
-    large_image = cv2.imread('./assets/ss.png')
-    result = cv2.matchTemplate(small_image, large_image, method)
-    mn,_,mnLoc,_ = cv2.minMaxLoc(result)
-    X, Y = mnLoc
+    X, Y, z, z = pyautogui.locateOnScreen('./assets/resize.png')
     
     # Drag the window all the way to the left
     pyautogui.moveTo(X + 50, Y)
@@ -65,14 +53,7 @@ def find_text_box():
     global text_box_y
 
     # Find the coords of the textbox asset
-    method = cv2.TM_SQDIFF_NORMED
-    small_image = cv2.imread('./assets/textbox.png')
-    large_image = cv2.imread('./assets/ss.png')
-    result = cv2.matchTemplate(small_image, large_image, method)
-    mn,_,mnLoc,_ = cv2.minMaxLoc(result)
-    
-    # Set global variables
-    text_box_x, text_box_y = mnLoc
+    text_box_x, text_box_y, z, z = pyautogui.locateOnScreen('./assets/textbox.png')
 
 
 def find_new_message():
@@ -83,14 +64,38 @@ def find_new_message():
     global new_message_y
 
     # Get spot coords
-    method = cv2.TM_SQDIFF_NORMED
-    small_image = cv2.imread('./assets/new_message.png')
-    large_image = cv2.imread('./assets/ss.png')
-    result = cv2.matchTemplate(small_image, large_image, method)
-    mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+    new_message_x, new_message_y, z, z = pyautogui.locateOnScreen('./assets/new_message.png')
+
+    # Convert numpy to into
+    new_message_x = numpy.int64(new_message_x).item()
+    new_message_y = numpy.int64(new_message_y).item()
     
-    # Set globals
-    new_message_x, new_message_y = mnLoc
+
+def read_more():
+    ''' Annoying function to click all possible locations of the read more button '''
+
+    # Background color, boolean, x-coordinates
+    black = (10, 12, 13)
+    done = False
+    xvals = [65, 150, 200]
+    
+    # Click all possible read more button locations until message doesn't get longer
+    while not done:
+        for val in xvals:
+            pyautogui.click(new_message_x + val, new_message_y - 45)
+            pyautogui.click(new_message_x + val, new_message_y - 65)
+
+        # If read more button is pressed the background color won't be present here
+        tryloop = True
+        while tryloop:
+            try:
+                if pyautogui.pixel(new_message_x + 10, new_message_y - 20) == black:
+                    done = True
+                else:
+                    pyautogui.press('end')
+                tryloop = False
+            except:
+                pass
 
 
 def write_message(message):
@@ -98,33 +103,15 @@ def write_message(message):
 
     # Click on the text box
     pyautogui.click(text_box_x,text_box_y)
-
+    
     # Base64encode message and append a * so receiver knows when we're done
-    b64message = base64.b64encode(message) + '*'.encode()
+    b64message = base64.b64encode(message)
+    pyperclip.copy(b64message.decode())
     
-    # Send the messages in 844 character chunks
-    while(len(b64message.decode("utf-8")) > 844):
-    
-        # make curr_chunk string the first 844 characters of b64message
-        curr_chunk = b64message[:844]
-        
-        # Remove the characters in the b64message string
-        b64message = b64message[844:]
-        
-        # write the message in the text box
-        pyautogui.write(curr_chunk.decode())
+    # Paste message into box
+    pyautogui.hotkey('ctrl', 'v')    
 
-        # Send the message
-        pyautogui.press('enter')
-        time.sleep(delay)
-        
-        # Wait for an ack before sending next chunk
-        wait_ack()
-
-    # write the message to the box
-    pyautogui.write(b64message.decode())
-    
-    # click send
+    # Send the message
     pyautogui.press('enter')
     time.sleep(delay)
 
@@ -135,11 +122,11 @@ def most_recent_sender():
     green = (5, 97, 98)
     grey = (38, 45, 49)
     black = (10, 12, 13)
-
+    
     # Try until it works because the pyautogui pixel function has a 50-50 chance of erroring
     while True:
         try:
-            # Checks if the pixel in the newest message is green (self), grey (server), black (ACK from either party)
+            # Checks if the pixel in the newest message is green (self), grey (server), black not a real message ignore
             color = pyautogui.pixel((new_message_x + 90), (new_message_y - 30))
             
             # Green message bubble, sent from self
@@ -150,9 +137,9 @@ def most_recent_sender():
             elif color == grey:
                 return "Server"
                 
-            # Black, background color, this means the message is short (has to be an ack)
+            # Black, background color, this means the message is short, not a real message
             elif color == black:
-                return "Ack"
+                return "TooShort"
                 
         # For when pyautogui breaks
         except:
@@ -165,11 +152,13 @@ def read_message():
     # Check who sent the most recent message
     sender = most_recent_sender()
 
-    # Don't care to read acks or self messages
-    if sender == "Self":
-        return None
-    elif sender == "Ack":
-        return None
+    # If no new message is available 
+    while sender != "Server":
+        time.sleep(delay)
+        sender = most_recent_sender()
+
+    # Fully expand message
+    read_more()
 
     # Highlight and copy new message
     highlight_new_message()
@@ -178,69 +167,19 @@ def read_message():
     
     # Return message from clipboard
     new_message = pyperclip.paste()
-    return new_message
 
+    # Images error on utf-8 decode, easiest way to figure out if we can decode is to try
+    try:
+        return base64.b64decode(new_message).decode()
+    except:
+        return base64.b64decode(new_message)
 
 
 def highlight_new_message():
     '''Highlights the new message'''
-
     pyautogui.click(new_message_x -11, new_message_y - 30)
     pyautogui.click(new_message_x -11, new_message_y - 30)
     pyautogui.click(new_message_x -11, new_message_y - 30)
-
-
-def wait_full_message():
-    '''Waits for the next full message to come in, ACKs and decodes as necessary'''
-
-    # Attempt to read a message
-    message = read_message()
-
-    # If no new message available try every DELAY seconds
-    while message is None or len(message) < 1:
-        time.sleep(delay)
-        message = read_message()
-
-    # If it's not a full message ([-1] != '*') read it as chunks
-    while message[-1] != '*':
-        
-        # Ack every chunk 
-        write_ack()
-        
-        # Call read_message until new chunk is available 
-        next_chunk = read_message()
-        while next_chunk is None:
-            time.sleep(delay)
-            next_chunk = read_message()
-            
-        # Concat chunks
-        message += next_chunk
-
-    # Images error on utf-8 decode, easiest way to figure out if we can decode is to try
-    try:
-        return base64.b64decode(message[:-1]).decode()
-    except:
-        return base64.b64decode(message[:-1])
-
-
-def wait_ack():
-    '''Waits for ACK'''
-    while most_recent_sender() != "Ack":
-        time.sleep(delay)
-
-
-def write_ack():
-    '''Send ACK'''
-    
-    # Click on the text box
-    pyautogui.click(text_box_x,text_box_y)
-    
-    # Write ACK
-    pyautogui.write("Ack")
-    
-    # click send
-    pyautogui.press('enter')
-    time.sleep(delay)
 
 
 def start():
@@ -251,9 +190,8 @@ def start():
         
         while True:
             conn, addr = s.accept()
-            print('Connection from', addr)
+            print('Connection from ', addr)
             data = conn.recv(BUFFER_SIZE)
-
             #if not data:
             #    break
             garbage = ["firefox", "pocket", "mozilla.com", "mozilla.net", "telem", "push.services", "safebrow"]
@@ -270,7 +208,7 @@ def tunnel_request(data, conn):
     time.sleep(.5)
 
     # Wait for the next message from the server
-    response = wait_full_message()
+    response = read_message()
     
     # Adds padding and forwards response to browser, bytes if needed
     try:
